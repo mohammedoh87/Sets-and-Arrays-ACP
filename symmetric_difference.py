8fd7e9e361c7b657f133aa37fea927ee8b747318
mohammedoh87 <aamina.oh87@gmail.com>
2024-10-25 16:10:37 +0300

    Sets and Arrays ACP

---
+++ symmetric_difference.py
@@ -0,0 +1,10 @@
+first_set = {1,2,3,4,5,6}
+second_set = {4,5,6,7,8,9}
+
+print(first_set)
+print(second_set)
+
+print("The symmetric difference of both sets is ")
+
+difference = first_set.symmetric_difference(second_set)
+print(difference)
